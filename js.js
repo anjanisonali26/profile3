f09a1e14a619d33a49e06d899a018d00111d448f
anjanisonali <anjanibendre260201@gmail.com>
2020-11-20 19:27:53 +0700

    update

---
+++ js.js
@@ -0,0 +1,24 @@
+$('.nav ul li').click(function(){
+    $(this).addClass("active").siblings().removeClass('active');
+})
+
+const tabBtn = document.querySelectorAll('.nav ul li')
+const tab = document.querySelectorAll('.tab');
+
+function tabs(panelIndex){
+    tab.forEach(function(node){
+        node.style.display = 'none';
+    });
+    tab[panelIndex].style.display = 'block';
+    
+}
+tabs(0);
+
+
+let bio = document.querySelectorAll('.bio');
+
+function bioText(){
+    bio.innerText = bio.innerText.substring(0, 100) + "...";
+    bio.innerHTML += "&nbsp" + '<span onclick>'
+}
+
